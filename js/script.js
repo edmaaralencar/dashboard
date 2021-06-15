bb7505090557c1134cd3be7444b6f76bbaa78787
Edmar Alencar <76753377+edmaaralencar@users.noreply.github.com>
2021-06-14 22:38:08 -0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,92 @@
+const navMenu = document.getElementById('menu-js')
+
+navMenu.addEventListener("click", () => {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('active')
+})
+
+document.querySelector(".search-input").placeholder = "Procurar";
+
+
+const date = new Date();
+
+const renderCalendar = () => {
+    date.setDate(1);
+
+    const monthDays = document.querySelector(".days");
+
+    const lastDay = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+    ).getDate();
+
+    const prevLastDay = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        0
+    ).getDate();
+
+    const firstDayIndex = date.getDay();
+
+    const lastDayIndex = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+    ).getDay();
+
+    const nextDays = 7 - lastDayIndex - 1;
+
+    const months = [
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julho",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro",
+    ];
+
+    document.querySelector(".date #month").innerHTML = months[date.getMonth()];
+
+    document.querySelector(".date #year").innerHTML = new Date().getFullYear().toString();
+
+    let days = "";
+
+    for (let x = firstDayIndex; x > 0; x--) {
+        days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+    }
+
+    for (let i = 1; i <= lastDay; i++) {
+        if (
+        i === new Date().getDate() &&
+        date.getMonth() === new Date().getMonth()
+        ) {
+        days += `<div class="today">${i}</div>`;
+        } else {
+        days += `<div>${i}</div>`;
+        }
+    }
+
+    for (let j = 1; j <= nextDays; j++) {
+        days += `<div class="next-date">${j}</div>`;
+        monthDays.innerHTML = days;
+    }
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+renderCalendar();
